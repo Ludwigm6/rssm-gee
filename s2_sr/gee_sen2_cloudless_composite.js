b8651f063447dbcb4966e794c0026ca48d5becad
marvin.ludwig93 <marvin.ludwig93@gmail.com>
2023-03-14 13:37:40 +0000

    Updated s2_sr/gee_sen2_cloudless_composite.js (added 3 lines, deleted 3 lines).

--- s2_sr/gee_sen2_cloudless_composite.js
+++ s2_sr/gee_sen2_cloudless_composite.js
@@ -12,11 +12,11 @@ function maskS2clouds(image) {
 // Map the function over the time period of data and take the median.
 var collection = ee.ImageCollection('COPERNICUS/S2_SR')
     .filterBounds(aoi)
-    .filterDate('2021-05-01', '2021-08-30')
+    .filterDate('2022-05-01', '2022-09-30')
     .map(maskS2clouds)
     .select('B2', 'B3', 'B4', 'B8')
 
-var composite = collection.median().mask(aoi)
+var composite = collection.median().clip(aoi)
 
 
 Map.addLayer(composite, {}, "Composite")
@@ -25,6 +25,6 @@ Map.addLayer(composite, {}, "Composite")
 Export.image.toDrive({
   image: composite,
   description: 'sentinel',
-  scale: 10,
+  scale: 50,
   region: aoi
 });
