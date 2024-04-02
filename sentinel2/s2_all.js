bae03097238780e47947e2b9b57e48703e36e8d9
marvin.ludwig93 <marvin.ludwig93@gmail.com>
2024-04-02 08:46:05 +0000

    Updated sentinel2/s2_all.js (added 27 lines).

--- sentinel2/s2_all.js
+++ sentinel2/s2_all.js
@@ -8,6 +8,21 @@
 var pois = ee.FeatureCollection("projects/ee-ludwigm6/assets/luenten/extent")
 var out_prefix = "luenten"
 
+
+
+var index = "NDVI"
+
+
+// na_value - numeric: NA value in the output table (e.g. for cloud pixel)
+var na_value = -5
+
+// Define start and end years and month
+var startYear = 1985;
+var endYear = 2023;
+var startMonth = 1;
+var endMonth = 12;
+
+
 // ###################################################################################
 // SCRIPT START
 
@@ -19,6 +34,18 @@ var dataset = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
                   .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE',80))
                   //.map(maskS2clouds);
 
+
+
+
+
+
+
+// Conduct filename
+var output = out_prefix + "-" + index + "-" +
+             startYear.toString() + "-" + endYear.toString() + "-" +
+             ("0" + startMonth).slice(-2) + "-" +
+             ("0" + endMonth).slice(-2)
+print(output)
 
 
 
