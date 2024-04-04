f6114364ad675809468ea23c1762c51d4d986fe4
marvin.ludwig93 <marvin.ludwig93@gmail.com>
2024-04-04 11:18:17 +0000

    Updated sentinel2/s2_all.js (added 13 lines).

--- sentinel2/s2_all.js
+++ sentinel2/s2_all.js
@@ -48,6 +48,19 @@ var ndvi = function(img){
   }
 
 
+function maskS2clouds(image) {
+  var qa = image.select('QA60');
+
+  // Bits 10 and 11 are clouds and cirrus, respectively.
+  var cloudBitMask = 1 << 10;
+  var cirrusBitMask = 1 << 11;
+
+  // Both flags should be set to zero, indicating clear conditions.
+  var mask = qa.bitwiseAnd(cloudBitMask).eq(0)
+      .and(qa.bitwiseAnd(cirrusBitMask).eq(0));
+
+  return image.updateMask(mask).divide(10000);
+}
 
 
 // cloud masking
